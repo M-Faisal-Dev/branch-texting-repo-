ca1c174a7c15358010b09113c795e1871a79564c
Muhammad Faisal <faisalgojra339@gmail.com>
2024-03-17 12:32:57 +0500

    going to push code from another folder

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 console.log("hello this is faisal and this is my first comment into github to check how github is working how branching is working something for doo this text i make is branch")
 
 console.log("hello this is faisal and i create a new branch its name is faisal")
+
+console.log("hello this is another folder")
