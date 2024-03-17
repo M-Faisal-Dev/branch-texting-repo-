fcae35a866b247f1eff21494046d740a0de53b1a
Muhammad Faisal <faisalgojra339@gmail.com>
2024-03-17 11:14:07 +0500

    push the code into faisal branch

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
 console.log("hello this is faisal and this is my first comment into github to check how github is working how branching is working something for doo this text i make is branch")
+
+console.log("hello this is faisal and i create a new branch its name is faisal")
